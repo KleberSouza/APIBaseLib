a31eccfdfc6a1485587610b59476e95911822171
Kleber Souza <kleberjfsouza@gmail.com>
2024-08-09 09:25:43 -0300

    :tada: Commit inicial

--- obj/Debug/net8.0/APIBaseLib.AssemblyInfo.cs
+++ obj/Debug/net8.0/APIBaseLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIBaseLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1770df8fe224c514bdb6f80182b7e3b66286a380")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIBaseLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIBaseLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
